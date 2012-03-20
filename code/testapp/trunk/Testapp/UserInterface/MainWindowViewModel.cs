457974d1e8460a08b44519c9a9a9720b5bbbd0ea
Christina Heidt <christina.heidt@elmer.ch>
2012-03-20 07:44:18 +0000

    refs #733
    Former-commit-id: 9ff60577cf11cee43aabc5041c51547e36115737

---
+++ code/testapp/trunk/Testapp/UserInterface/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using Services;
+
+namespace UserInterface
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public MainWindowViewModel(IPostersService postersService)
+        {
+            Posters = new ListCollectionView(postersService.Posters);
+        }
+
+        private void Notify(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ListCollectionView Posters { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
