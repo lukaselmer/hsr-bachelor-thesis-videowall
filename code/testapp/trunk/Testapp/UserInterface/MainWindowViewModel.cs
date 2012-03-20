2cb4d2219a6e870fe05e1790ec73fbf36724fa9d
Delia Treichler <delia.teichler@elmer.ch>
2012-03-20 14:51:20 +0000

    refs #733
    Former-commit-id: 7e21502a40801eaa193401314b8509273489cf2b

--- code/testapp/trunk/Testapp/UserInterface/MainWindowViewModel.cs
+++ code/testapp/trunk/Testapp/UserInterface/MainWindowViewModel.cs
@@ -4,25 +4,64 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Windows.Data;
+using System.Windows.Input;
 using Services;
 
 namespace UserInterface
 {
     public class MainWindowViewModel : INotifyPropertyChanged
     {
+        //Declarations
+        private IPoster _currentPoster;
+        private IList<IPoster> _posters; 
+        
+
+        //Constructor
         public MainWindowViewModel(IPostersService postersService)
         {
-            Posters = new ListCollectionView(postersService.Posters);
+            _posters = postersService.Posters;
+            CurrentPoster = _posters.First();
+            NavigateToLeftCommand = new Command(OnNavigateToLeft);
+            NavigateToRightCommand = new Command(OnNavigateToRight);
         }
 
+
+        //Notify
+        public event PropertyChangedEventHandler PropertyChanged;
         private void Notify(string propertyName)
         {
             if (PropertyChanged != null)
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        public ListCollectionView Posters { get; set; }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        //Properties
+       public IPoster CurrentPoster
+        {
+            get { return _currentPoster; }
+            private set 
+            { 
+                _currentPoster = value;
+                Notify("CurrentPoster");
+            }
+        }
+
+        //Commands
+        public ICommand NavigateToLeftCommand { get; private set; } 
+        public ICommand NavigateToRightCommand { get; private set; }
+
+
+        private void OnNavigateToLeft(object obj)
+        {
+            int index = _posters.IndexOf(CurrentPoster) - 1;
+            CurrentPoster = index < 0 ? _posters.Last() : _posters[index];
+            Notify("CurrentPoster");
+        }
+        private void OnNavigateToRight(object obj)
+        {
+            int index = _posters.IndexOf(CurrentPoster) + 1;
+            CurrentPoster = index >= _posters.Count ? _posters.First() : _posters[index];
+            Notify("CurrentPoster");
+        }
     }
 }
