2b6618621641e2d791c7c0a72f61828455484e6b
Lukas Elmer <lukas.elmer@gmail.com>
2012-03-23 16:06:51 +0000

    Hand cursor demo app
    Former-commit-id: beb920451e1c0432193d7aabe0031282d6261637

---
+++ code/miniapps/trunk/HandCursorDemoApp/HandCursorDemoApp/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace HandCursorDemoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Cursor = Cursors.None;
+        }
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            var pos = e.GetPosition(MyGrid);
+            Hand.Margin = new Thickness(pos.X - 80, pos.Y - 80, 0, 0);
+        }
+    }
+}
