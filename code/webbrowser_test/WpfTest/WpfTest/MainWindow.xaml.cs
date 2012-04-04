d55ea945e7e25edf0b5ca69110658f20e8935e71
Christina Heidt <christina.heidt@elmer.ch>
2012-04-04 13:19:54 +0000

    refs #751 Webbrowsertest
    Former-commit-id: 98b27b20ce02ccc8ef2d91904054c85147b0b944

---
+++ code/webbrowser_test/WpfTest/WpfTest/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _path;
+        private List<string> _files;
+        private int currentFileIndex;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _path = @"D:\Video Wall\doc\media\poster\Beispielposter\";
+            _files = new List<string>
+                               {
+                                   "Poster_Android.pdf", "Poster_Bodyscanner.pdf", "Poster_EventAssistant.pdf", "Poster_Exchange.pdf",
+                                   "Poster_FireTabletPlus.pdf", "Poster_iPhone.pdf", "Poster_Isochrones.pdf", "Poster_Lumin.pdf", "Poster_SnapIt.pdf"
+                               };
+            currentFileIndex = 0;
+            Navigate();
+        }
+
+        private void Navigate()
+        {
+            webBrowser.Navigate(new Uri(_path + _files[currentFileIndex], UriKind.RelativeOrAbsolute));
+        }
+
+        private void Next()
+        {
+            currentFileIndex = (currentFileIndex + 1 == _files.Count) ? 0 : currentFileIndex + 1;
+        }
+
+        private void Previous()
+        {
+            currentFileIndex = (currentFileIndex - 1 < 0) ? _files.Count - 1 : currentFileIndex - 1;
+        }
+
+        private void PreviousClick(object sender, RoutedEventArgs e)
+        {
+            Previous();
+            Navigate();
+        }
+
+        private void NextClick(object sender, RoutedEventArgs e)
+        {
+            Next();
+            Navigate();
+        }
+    }
+}
