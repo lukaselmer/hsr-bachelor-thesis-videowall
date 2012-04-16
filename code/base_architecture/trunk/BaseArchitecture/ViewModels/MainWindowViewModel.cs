8bcb80f2bf6b89a32d4a6b8ea76ebe813bc1b6eb
Delia Treichler <delia.teichler@elmer.ch>
2012-04-16 13:52:09 +0000

    refs #760 MenuViewModel erstellt
    Former-commit-id: 8bffea57597c8c74b37e3d7f94d69d0ee2bb6dc7

--- code/base_architecture/trunk/BaseArchitecture/ViewModels/MainWindowViewModel.cs
+++ code/base_architecture/trunk/BaseArchitecture/ViewModels/MainWindowViewModel.cs
@@ -4,37 +4,15 @@ namespace ViewModels
 {
     public class MainWindowViewModel
     {
-        public MainWindowViewModel(PosterViewModel posterViewModel, LunchMenuViewModel lunchMenuViewModel, PlayerViewModel playerViewModel, ICursorViewModel cursorViewModel)
+        public MainWindowViewModel(MenuViewModel menuViewModel, PlayerViewModel playerViewModel, ICursorViewModel cursorViewModel)
         {
-            PosterViewModel = posterViewModel;
-            LunchMenuViewModel = lunchMenuViewModel;
+            MenuViewModel = menuViewModel;
             PlayerViewModel = playerViewModel;
             CursorViewModel = cursorViewModel;
-            ShowPosterViewCommand = new Command(OnShowPosterView);
-            ShowLunchMenuViewCommand = new Command(OnShowLunchMenuView);
-            OnShowPosterView(null);
         }
 
-        private void OnShowPosterView(object obj)
-        {
-            LunchMenuViewModel.IsLunchMenuViewVisible = false;
-            PosterViewModel.IsPosterViewVisible = true;
-        }
-
-        private void OnShowLunchMenuView(object obj)
-        {
-            PosterViewModel.IsPosterViewVisible = false;
-            LunchMenuViewModel.IsLunchMenuViewVisible = true;
-        }
-
-
-        public PosterViewModel PosterViewModel { get; set; }
-        public LunchMenuViewModel LunchMenuViewModel { get; set; }
+        public MenuViewModel MenuViewModel { get; set; }
         public PlayerViewModel PlayerViewModel { get; set; }
         public ICursorViewModel CursorViewModel { get; set; }
-
-        public ICommand ShowLunchMenuViewCommand { get; set; }
-
-        public ICommand ShowPosterViewCommand { get; set; }
     }
 }
---
+++ code/base_architecture/trunk/BaseArchitecture/ViewModels/MenuViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace ViewModels
+{
+    public class MenuViewModel
+    {
+        public MenuViewModel(PosterViewModel posterViewModel, LunchMenuViewModel lunchMenuViewModel)
+        {
+            PosterViewModel = posterViewModel;
+            LunchMenuViewModel = lunchMenuViewModel;
+            ShowPosterViewCommand = new Command(OnShowPosterView);
+            ShowLunchMenuViewCommand = new Command(OnShowLunchMenuView);
+            OnShowPosterView(null);
+        }
+
+        public PosterViewModel PosterViewModel { get; set; }
+        public LunchMenuViewModel LunchMenuViewModel { get; set; }
+
+        private void OnShowPosterView(object obj)
+        {
+            LunchMenuViewModel.IsLunchMenuViewVisible = false;
+            PosterViewModel.IsPosterViewVisible = true;
+        }
+
+        private void OnShowLunchMenuView(object obj)
+        {
+            PosterViewModel.IsPosterViewVisible = false;
+            LunchMenuViewModel.IsLunchMenuViewVisible = true;
+        }
+        public ICommand ShowLunchMenuViewCommand { get; set; }
+
+        public ICommand ShowPosterViewCommand { get; set; }
+    }
+}
