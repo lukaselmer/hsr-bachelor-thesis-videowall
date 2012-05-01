1ad1255c6c9fb01553c4828c6acb9fbd2d8f97f5
Lukas Elmer <lukas.elmer@gmail.com>
2012-05-01 10:23:58 +0000

    refs #790
    Former-commit-id: 1ffd3771159f610231fed09a196bec5e19872770

---
+++ code/video_wall/tags/sp09/BaseArchitecture/Views/MenuView.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace Views
+{
+    /// <summary>
+    /// Interaction logic for MenuView.xaml
+    /// </summary>
+    public partial class MenuView
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MenuView"/> class.
+        /// </summary>
+        public MenuView()
+        {
+            InitializeComponent();
+        }
+    }
+}
