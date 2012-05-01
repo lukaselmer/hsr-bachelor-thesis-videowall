4aa4c03b4e2898fd9f96f1808f66de20015906ef
Lukas Elmer <lukas.elmer@gmail.com>
2012-05-01 10:26:10 +0000

    refs #789
    Former-commit-id: a2a696a01e7bf01b59aa03fff64965a89ce40372

---
+++ code/video_wall/tags/sp08/BaseArchitecture/Views/SkeletonLinesView.xaml.cs
@@ -0,0 +1,16 @@
+namespace Views
+{
+    /// <summary>
+    /// Interaction logic for SkeletonlinesView.xaml
+    /// </summary>
+    public partial class SkeletonLinesView
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SkeletonLinesView"/> class.
+        /// </summary>
+        public SkeletonLinesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
