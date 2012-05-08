072df1ccc28f5466a680591d5752a58d33f1342c
Lukas Elmer <lukas.elmer@gmail.com>
2012-05-08 14:21:42 +0000

    refs #839
    Former-commit-id: aa51e353a43079b0c60cb21fcda3bfefc845c02f

--- code/video_wall/trunk/VideoWall/Services/Apps/AppController.cs
+++ code/video_wall/trunk/VideoWall/Services/Apps/AppController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel.Composition;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using Common;
 using Interfaces;
 
 namespace Services.Apps
@@ -13,15 +15,21 @@ namespace Services.Apps
     /// </summary>
     public class AppController
     {
+        /// <summary>
+        /// The video wall applications
+        /// </summary>
         [ImportMany(AllowRecomposition = true)]
         public ObservableCollection<IApp> Apps { get; private set; }
 
         /// <summary>
-        /// TODO
+        /// Initializes the AppController
         /// </summary>
         public AppController()
         {
             ExtensionManager.Init(this);
+            PreCondition.AssertNotNull(Apps, "Apps");
+            // At least one app has to be loaded
+            PreCondition.AssertInRange(1, Int32.MaxValue, Apps.Count, "Apps.Count");
         }
     }
 }
