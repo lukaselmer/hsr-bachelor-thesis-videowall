739b1c2457e1ede9f38f97ccca89236ea3ea5b0e
Delia Treichler <delia.teichler@elmer.ch>
2012-05-11 13:42:27 +0000

    refs #800
    Former-commit-id: 9b5a6c967babf9f43ef4cc7aaecc6b7437a3e874

---
+++ code/video_wall/trunk/LunchMenuApp/LunchMenuApp/LunchMenuApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using Interfaces;
+using Views.Xaml;
+
+namespace LunchMenuApp
+{
+    [Export(typeof(IApp))]
+    public class LunchMenuApp : IApp
+    {
+        public LunchMenuApp()
+        {
+            Name = "Mittagsmenü";
+            MainView = new LunchMenuView();
+        }
+        public UserControl MainView { get; private set; }
+        public string Name { get; private set; }
+    }
+}
