a48f1583622791e7e3fa517f1ca952253e20632b
Delia Treichler <delia.teichler@elmer.ch>
2012-05-14 12:28:16 +0000

    refs #836 IApp implement members: DemomodeText
    Former-commit-id: 10e971d51730eca27ce5d4760bf2efbffb4d7742

--- code/video_wall/trunk/PosterApp/PosterApp/PosterApp.cs
+++ code/video_wall/trunk/PosterApp/PosterApp/PosterApp.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Composition;
-using System.Linq;
-using System.Text;
+﻿using System.ComponentModel.Composition;
 using System.Windows.Controls;
 using Interfaces;
 using Views.Xaml;
@@ -10,15 +6,17 @@ using Views.Xaml;
 namespace PosterApp
 {
     [Export(typeof(IApp))]
-    class PosterApp : IApp
+    public class PosterApp : IApp
     {
         public PosterApp()
         {
             Name = "Posters";
             MainView = new PosterView();
+            DemomodeText = "Lust etwas zu lernen?";
         }
 
         public UserControl MainView { get; private set; }
         public string Name { get; private set; }
+        public string DemomodeText { get; private set; }
     }
 }
