2c23e0e1c1513538958514a1df1a2e6321879c30
Lukas Elmer <lukas.elmer@gmail.com>
2012-05-22 08:17:18 +0000

    refs #858
    Former-commit-id: 981f7a0800f50f853f90c563f4f0f236d6a0cd5d

--- code/video_wall/trunk/VideoWall/VideoWall.ViewModels/Menu/AppViewModel.cs
+++ code/video_wall/trunk/VideoWall/VideoWall.ViewModels/Menu/AppViewModel.cs
@@ -15,6 +15,7 @@
 
 #region Usings
 
+using System.Windows.Media;
 using VideoWall.Common;
 using VideoWall.Interfaces;
 
@@ -28,6 +29,9 @@ namespace VideoWall.ViewModels.Menu
     public class AppViewModel : Notifier
     {
         #region Declarations
+
+        private static readonly MenuStyleViewModel SelectedMenuStyle = new MenuStyleViewModel(Colors.Black, Color.FromRgb(0xEE, 0xEE, 0xEE), Color.FromRgb(0xFF, 0xFF, 0xFF));
+        private static readonly MenuStyleViewModel UnselectedMenuStyle = new MenuStyleViewModel(Colors.White, Color.FromRgb(0x00, 0x7D, 0xFF), Color.FromRgb(0x0F, 0x37, 0x60));
 
         private IApp _app;
         private bool _selected;
@@ -60,7 +64,19 @@ namespace VideoWall.ViewModels.Menu
             {
                 _selected = value;
                 Notify("Selected");
+                Notify("MenuStyle");
             }
+        }
+
+        /// <summary>
+        /// Gets or sets the menu style.
+        /// </summary>
+        /// <value>
+        /// The menu style.
+        /// </value>
+        public MenuStyleViewModel MenuStyle
+        {
+            get { return Selected ? SelectedMenuStyle : UnselectedMenuStyle; }
         }
 
         #endregion
@@ -77,5 +93,48 @@ namespace VideoWall.ViewModels.Menu
         }
 
         #endregion
+    }
+
+    /// <summary>
+    /// The menu style view model defines the style for the menu
+    /// </summary>
+    public class MenuStyleViewModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MenuStyleViewModel"/> class.
+        /// </summary>
+        /// <param name="fontColor">Color of the font.</param>
+        /// <param name="colorTop">Color of the light.</param>
+        /// <param name="colorBottom">Color of the dark.</param>
+        public MenuStyleViewModel(Color fontColor, Color colorTop, Color colorBottom)
+        {
+            FontColor = fontColor;
+            LightColor = colorTop;
+            DarkColor = colorBottom;
+        }
+
+        /// <summary>
+        /// Gets the color of the font.
+        /// </summary>
+        /// <value>
+        /// The color of the font.
+        /// </value>
+        public Color FontColor { get; private set; }
+
+        /// <summary>
+        /// Gets the light color for the menu button
+        /// </summary>
+        /// <value>
+        /// The color of the light.
+        /// </value>
+        public Color LightColor { get; private set; }
+
+        /// <summary>
+        /// Gets the dark color for the menu button
+        /// </summary>
+        /// <value>
+        /// The color of the dark.
+        /// </value>
+        public Color DarkColor { get; private set; }
     }
 }
