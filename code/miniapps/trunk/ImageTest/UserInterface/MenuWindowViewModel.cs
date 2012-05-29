8b784bee4a99b179565c8372520cfbc77d7983f8
Christina Heidt <christina.heidt@elmer.ch>
2012-05-29 12:31:07 +0000

    refs #885 Apps umbenennen und neu anordnen
    Former-commit-id: 6fd21c2cec974ee9f20ae8413c1f2f1f38fbd80f

---
+++ code/miniapps/trunk/ImageTest/UserInterface/MenuWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Services;
+
+namespace UserInterface
+{
+    public class MenuWindowViewModel : INotifyPropertyChanged
+    {
+        private bool _isMenuViewVisible;
+
+        public MenuWindowViewModel(MenuService menuService)
+        {
+            CurrentMenu = menuService.Menu;
+        }
+
+        public Menu CurrentMenu { get; set; }
+
+        public bool IsMenuViewVisible
+        {
+            get
+            {
+                return _isMenuViewVisible;
+            }
+            set
+            {
+                _isMenuViewVisible = value;
+                Notify("IsMenuViewVisible");
+            }
+        }
+
+        //Notify
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void Notify(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
