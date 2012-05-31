64d3ea33f383b1406a92d6a68e82c9fd15573eca
Delia Treichler <delia.teichler@elmer.ch>
2012-05-31 14:51:35 +0000

    refs #889 Namespace documentation
    Former-commit-id: c5be9479d54c3b7efab8e6f845c820f20f0b10dd

---
+++ code/video_wall/trunk/PosterApp/PosterApp.ServiceModels/NamespaceDoc.cs
@@ -0,0 +1,35 @@
+﻿#region Header
+
+// ------------------------ Licence / Copyright ------------------------
+// 
+// HSR Video Wall
+// Copyright © Lukas Elmer, Christina Heidt, Delia Treichler
+// All Rights Reserved
+// 
+// Authors:
+// Lukas Elmer, Christina Heidt, Delia Treichler
+// 
+// ---------------------------------------------------------------------
+
+#endregion
+
+#region Usings
+
+using System.Runtime.CompilerServices;
+
+#endregion
+
+namespace PosterApp.ServiceModels
+{
+    /// <summary>
+    ///   The <see cref="PosterApp.ServiceModels" /> namespace contains service classes.
+    /// </summary>
+    [CompilerGenerated]
+    // ReSharper disable UnusedMember.Global
+    //This class is necessary to comment the namespace
+    //PosterApp.ServiceModels but ReSharper marks this class as unused.
+    internal class NamespaceDoc
+        // ReSharper restore UnusedMember.Global
+    {
+    }
+}
---
+++ code/video_wall/trunk/PosterApp/PosterApp/NamespaceDoc.cs
@@ -0,0 +1,35 @@
+﻿#region Header
+
+// ------------------------ Licence / Copyright ------------------------
+// 
+// HSR Video Wall
+// Copyright © Lukas Elmer, Christina Heidt, Delia Treichler
+// All Rights Reserved
+// 
+// Authors:
+// Lukas Elmer, Christina Heidt, Delia Treichler
+// 
+// ---------------------------------------------------------------------
+
+#endregion
+
+#region Usings
+
+using System.Runtime.CompilerServices;
+
+#endregion
+
+namespace PosterApp.Main
+{
+    /// <summary>
+    ///   The <see cref="PosterApp.Main" /> namespace contains the main.
+    /// </summary>
+    [CompilerGenerated]
+    // ReSharper disable UnusedMember.Global
+    //This class is necessary to comment the namespace
+    //PosterApp.Main but ReSharper marks this class as unused.
+    internal class NamespaceDoc
+        // ReSharper restore UnusedMember.Global
+    {
+    }
+}
