c0e988a4e648c509f5c86b4e24e7d5435a7d1f60
Lukas Elmer <lukas.elmer@gmail.com>
2012-06-01 11:21:53 +0000

    refs #889 Refactoring
    Former-commit-id: e79bf55987548a449a49a0e4556f74c3ceaa94f2

---
+++ code/video_wall/trunk/VideoWall/VideoWall.Main/NamespaceDoc.cs
@@ -0,0 +1,35 @@
+﻿#region Header
+
+// ------------------------ Licence / Copyright ------------------------
+// 
+// HSR Video Wall
+// Copyright © Lukas Elmer, Christina Heidt, Delia Treichler
+// All Rights Reserved
+// 
+// Authors:
+// Lukas Elmer, Christina Heidt, Delia Treichler
+// 
+// ---------------------------------------------------------------------
+
+#endregion
+
+#region Usings
+
+using System.Runtime.CompilerServices;
+
+#endregion
+
+namespace VideoWall.Main
+{
+    /// <summary>
+    ///   The <see cref="VideoWall.Main" /> namespace contains the startup routine and the configuration for the application. It starts the main view.
+    /// </summary>
+    [CompilerGenerated]
+    // ReSharper disable UnusedMember.Global
+    //This class is necessary to comment the namespace
+    //VideoWall.Common but ReSharper marks this class as unused.
+    internal class NamespaceDoc
+    // ReSharper restore UnusedMember.Global
+    {
+    }
+}
