f9173617354d62bfcfdde470a62662fe6c3f460e
Lukas Elmer <lukas.elmer@gmail.com>
2012-06-06 16:32:39 +0000

    refs #899 Unit Tests
    Former-commit-id: 4d110777bd6f5d6adc88a9a04337f54f779fdcbe

--- code/video_wall/trunk/LunchMenuApp/LunchMenuApp.Tests/Data/LunchMenuReaderTest.cs
+++ code/video_wall/trunk/LunchMenuApp/LunchMenuApp.Tests/Data/LunchMenuReaderTest.cs
@@ -1,44 +1,40 @@
-﻿#region Header
-
-// ------------------------ Licence / Copyright ------------------------
-// 
-// HSR Video Wall
-// Copyright © Lukas Elmer, Christina Heidt, Delia Treichler
-// All Rights Reserved
-// 
-// Authors:
-// Lukas Elmer, Christina Heidt, Delia Treichler
-// 
-// ---------------------------------------------------------------------
-
-#endregion
-
-#region Usings
-
+﻿using System.Net.NetworkInformation;
 using LunchMenuApp.Data.Implementation;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-#endregion
+using System;
 
 namespace LunchMenuApp.Tests.Data
 {
-    ///<summary>
-    ///  This is a test class for LunchMenuReaderTest and is intended to contain all LunchMenuReaderTest Unit Tests.
+
+
+    /// <summary>
+    ///This is a test class for LunchMenuReaderTest and is intended
+    ///to contain all LunchMenuReaderTest Unit Tests
     ///</summary>
     [TestClass]
     public class LunchMenuReaderTest
     {
-        ///<summary>
-        ///  A test for DownloadHtml.
+        /// <summary>
+        /// A test for LunchMenuReader Constructor
+        /// This test is a little "stupid" because it only works if the http server of the sa mensa
+        /// is online. But: if the test does not work, the menu cannot be downloaded and the app will
+        /// not work. This is why this test makes sense.
+        /// If the computer is online, we assume that google.com can be reached.
         ///</summary>
         [TestMethod]
-        [DeploymentItem("LunchMenuApp.Data.dll")]
-        public void DownloadHtmlTest()
+        public void LunchMenuReaderConstructorTest()
         {
-            var reader = new LunchMenuReader_Accessor();
-            var html = reader.Html;
-            Assert.IsFalse(html.Length == 0);
-            Assert.IsTrue(html.Contains("Menuplan"));
+            if (!Online()) Assert.Inconclusive("You do not seem to be online, therefore this test cannot be executed.");
+
+            var lunchMenuReader = new LunchMenuReader();
+            Assert.IsFalse(String.IsNullOrEmpty(lunchMenuReader.Html));
+            Assert.IsTrue(lunchMenuReader.Html.Contains("<html"));
+        }
+
+        private static bool Online()
+        {
+            var pingReply = new Ping().Send("google.com", 700);
+            return pingReply != null && pingReply.Status == IPStatus.Success;
         }
     }
 }
