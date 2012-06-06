1acebf52ec68b5b25adbf4a1ad47bbb42cdae493
Delia Treichler <delia.teichler@elmer.ch>
2012-06-06 14:49:31 +0000

    refs #895 VideoWall fertig dokumentiert
    Former-commit-id: 83abba9bf22742c1f621489a7bc1dc13831a3584

--- code/video_wall/trunk/VideoWall/VideoWall.Interfaces/IVideoWallServiceProvider.cs
+++ code/video_wall/trunk/VideoWall/VideoWall.Interfaces/IVideoWallServiceProvider.cs
@@ -28,8 +28,8 @@ namespace VideoWall.Interfaces
         /// <summary>
         ///   Gets an implementation of the interface which is provided by the video wall.
         /// </summary>
-        /// <typeparam name="T"> </typeparam>
-        /// <returns> </returns>
+        /// <typeparam name="T"> The interface of a specific video wall service. </typeparam>
+        /// <returns> A video wall service implementation. </returns>
         // ReSharper disable UnusedMember.Global
         // This method is used by the extensions. Since they are not in this project, ReSharper cannot know that this method is used.
         T GetExtension<T>() where T : IVideoWallService;
