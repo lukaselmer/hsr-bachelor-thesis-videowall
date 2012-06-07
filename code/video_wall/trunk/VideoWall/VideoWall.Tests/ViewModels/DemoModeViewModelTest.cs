81c8eb72202f0c84eba7e7760e620947aed4b351
Lukas Elmer <lukas.elmer@gmail.com>
2012-06-07 02:06:46 +0000

    refs #899 Unit tests
    Former-commit-id: 304f2b1471c75f86d40239a380a3b622f3aa80b0

---
+++ code/video_wall/trunk/VideoWall/VideoWall.Tests/ViewModels/DemoModeViewModelTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using VideoWall.ServiceModels.Apps.Implementation;
+using VideoWall.ServiceModels.DemoMode.Implementation;
+using VideoWall.Tests.Mocks;
+using VideoWall.Tests.ServiceModels.Apps;
+using VideoWall.ViewModels.DemoMode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VideoWall.ViewModels.Skeletons;
+using VideoWall.ViewModels.Menu;
+using VideoWall.ServiceModels.DemoMode.Interfaces;
+
+namespace VideoWall.Tests.ViewModels
+{
+    /// <summary>
+    ///This is a test class for DemoModeViewModelTest and is intended
+    ///to contain all DemoModeViewModelTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class DemoModeViewModelTest
+    {
+        /// <summary>
+        ///A test for DemoModeViewModel Constructor
+        ///</summary>
+        [TestMethod()]
+        public void DemoModeViewModelConstructorTest()
+        {
+            var player = new PlayerMock();
+
+            var extensionsFolder = FileDirectoryForTests.TestFilePrefix + "Extensions";
+            var extensionsConfig = new ExtensionsConfig(extensionsFolder);
+            var controller = new AppController(player, extensionsConfig);
+
+            var playerViewModel = new PlayerViewModel(new PlayerMock());
+            var menuViewModel = new MenuViewModel(controller);
+
+            var t = TimeSpan.FromDays(1);
+            var demoModeConfig = new DemoModeConfig(new[] { Colors.Red, Colors.Blue }, t, t, t, t, t);
+            var demoModeService = new DemoModeService(player, demoModeConfig);
+
+            var vm = new DemoModeViewModel(playerViewModel, menuViewModel, demoModeService);
+            Assert.AreEqual(Visibility.Collapsed, vm.CountDownVisibility);
+            Assert.AreEqual(Visibility.Collapsed, vm.DemoModeVisibility);
+            Assert.AreEqual(Visibility.Collapsed, vm.TeaserVisibility);
+            
+            Assert.AreSame(playerViewModel, vm.PlayerViewModel);
+        }
+    }
+}
