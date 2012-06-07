2722a01ab3fc730236467226cdf66be9f4533088
Delia Treichler <delia.teichler@elmer.ch>
2012-06-07 06:14:58 +0000

    refs #895 LunchMenuApp
    Former-commit-id: 517eaa330efa5f2307bb3d08c2d08eadc824f127

--- code/video_wall/trunk/LunchMenuApp/LunchMenuApp.Data/Implementation/WebDownloader.cs
+++ code/video_wall/trunk/LunchMenuApp/LunchMenuApp.Data/Implementation/WebDownloader.cs
@@ -1,21 +1,43 @@
-﻿using System.Net;
+﻿#region Header
+
+// ------------------------ Licence / Copyright ------------------------
+// 
+// HSR Video Wall
+// Copyright © Lukas Elmer, Christina Heidt, Delia Treichler
+// All Rights Reserved
+// 
+// Authors:
+// Lukas Elmer, Christina Heidt, Delia Treichler
+// 
+// ---------------------------------------------------------------------
+
+#endregion
+
+#region Usings
+
+using System.Net;
 using System.Text;
 using LunchMenuApp.Data.Interfaces;
+
+#endregion
 
 namespace LunchMenuApp.Data.Implementation
 {
     /// <summary>
-    /// The web downloader loads down a web sites HTML
+    ///   The web downloader downloads a websites HTML.
     /// </summary>
     // ReSharper disable ClassNeverInstantiated.Global
     // Created by unity, so ReSharper thinks this class is can be made abstract, which is wrong.
     public class WebDownloader : IUrlDownloader
-    // ReSharper restore ClassNeverInstantiated.Global
+        // ReSharper restore ClassNeverInstantiated.Global
     {
+        #region IUrlDownloader Members
+
         /// <summary>
         ///   Downloads the HTML.
         /// </summary>
         /// <param name="url"> The URL. </param>
+        /// <returns> The HTML as string. </returns>
         public string Download(string url)
         {
             using (var client = new WebClient())
@@ -24,5 +46,7 @@ namespace LunchMenuApp.Data.Implementation
                 return client.DownloadString(url);
             }
         }
+
+        #endregion
     }
 }
--- code/video_wall/trunk/LunchMenuApp/LunchMenuApp.Data/Interfaces/IUrlDownloader.cs
+++ code/video_wall/trunk/LunchMenuApp/LunchMenuApp.Data/Interfaces/IUrlDownloader.cs
@@ -1,15 +1,30 @@
+#region Header
+
+// ------------------------ Licence / Copyright ------------------------
+// 
+// HSR Video Wall
+// Copyright © Lukas Elmer, Christina Heidt, Delia Treichler
+// All Rights Reserved
+// 
+// Authors:
+// Lukas Elmer, Christina Heidt, Delia Treichler
+// 
+// ---------------------------------------------------------------------
+
+#endregion
+
 namespace LunchMenuApp.Data.Interfaces
 {
     /// <summary>
-    /// The interface for the web downloader
+    ///   The interface for the web downloader
     /// </summary>
     public interface IUrlDownloader
     {
         /// <summary>
-        /// Downloads the specified URL.
+        ///   Downloads the specified URL.
         /// </summary>
-        /// <param name="url">The URL.</param>
-        /// <returns></returns>
+        /// <param name="url"> The URL. </param>
+        /// <returns> The HTML as string. </returns>
         string Download(string url);
     }
 }
