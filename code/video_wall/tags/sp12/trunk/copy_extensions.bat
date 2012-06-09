8c795fc3f5dde7cd97390599d4ef35c645600ea3
Lukas Elmer <lukas.elmer@gmail.com>
2012-06-09 23:49:22 +0000

    Tag sp12
    Former-commit-id: e8b8782dbedc8c4416465568f09f979688fc644d

--- code/video_wall/tags/sp12/trunk/copy_extensions.bat
+++
@@ -1,3 +0,0 @@
-xcopy /E /H /R /Y .\PosterApp\PosterApp\bin\Debug .\VideoWall\Extensions
-xcopy /E /H /R /Y .\LunchMenuApp\LunchMenuApp\bin\Debug .\VideoWall\Extensions
-xcopy /E /H /R /Y .\DiagnosticsApp\DiagnosticsApp\bin\Debug .\VideoWall\Extensions
